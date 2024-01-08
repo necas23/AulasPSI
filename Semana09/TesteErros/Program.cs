cc8749c1bcce019ac5a75a47716a8401e1586852
Replit user <>
2024-01-08 13:07:19 +0000

    .

---
+++ Semana09/TesteErros/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace TesteErros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.Write("giInsere um número inteiro: ");
+                int i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Número inserido: {i}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu o seguinte problema: " + e.Message);
+            }
+        }
+    }
+}
